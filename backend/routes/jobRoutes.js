d0de518904628319dd60f8b208e8061149c09bc9
Sina Mussie <ninu@Sinas-MacBook-Air-3.local>
2026-05-15 01:04:16 +0200

    feat: protect job creation route

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -1,14 +1,21 @@
 import express from "express";
-const router = express.Router(); 
+const router = express.Router();
 
 import {
-    createJob,
-    getJobs,
+  createJob,
+  getJobs,
 } from "../controllers/jobController.js";
 
+import { protect, authorizeRoles } from "../middleware/authMiddleware.js";
 
-//CRUD routes 
-router.post("/", createJob);
+// CRUD routes
 router.get("/", getJobs);
+
+router.post(
+  "/",
+  protect,
+  authorizeRoles("company", "admin"),
+  createJob
+);
 
 export default router;

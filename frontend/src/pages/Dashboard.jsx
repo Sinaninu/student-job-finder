364701ab55d4ab2bc706408e8f421d8cc8c09ffa
Ssadiia-Omer <sadia.omer0005@stud.hkr.se>
2026-05-16 23:11:40 +0200

    chore: install axios and update Dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,7 +5,7 @@ import Navbar from '../components/Navbar'
 export default function Dashboard(){
   return(
     <div>
-      <Navbar/>
+      
       <div className='container'>
         <h2>Dashboard</h2>
         <p>Welcome user</p>
